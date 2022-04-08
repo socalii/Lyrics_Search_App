c9dfa7226d43f6e5394cca45569d8bbe0e5189c5
Susan Choi <2539lee@gmail.com>
2022-04-07 23:57:37 -0700

    Update DOM & Add Pagination

--- script.js
+++ script.js
@@ -10,7 +10,62 @@ async function searchSongs(term) {
   const res = await fetch(`${apiURL}/suggest/${term}`)
   const data = await res.json()
 
-  console.log(data)
+  showData(data)
+}
+
+// Show song and artist in DOM
+function showData(data) {
+  // let output = ''
+  // data.data.forEach((song) => {
+  //   output += `
+  //     <li>
+  //       <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //       <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+  //     </li>
+  //   `
+  // })
+  // result.innerHTML = `
+  //   <ul class="songs">
+  //     ${output}
+  //   </ul>
+  // `
+
+  result.innerHTML = `
+  <ul class="songs">
+    ${data.data
+      .map(
+        (song) => `<li>
+    <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+    <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+  </li>`
+      )
+      .join('')}
+  </ul>
+  `
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onClick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ''
+      }
+      ${
+        data.next
+          ? `<button class="btn" onClick="getMoreSongs('${data.next}')">Next</button>`
+          : ''
+      }
+    `
+  } else {
+    more.innerHTML = ''
+  }
+}
+
+// Get prev and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+  const data = await res.json()
+
+  showData(data)
 }
 
 // Event listeners
